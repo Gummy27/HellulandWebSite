a4694e290607fdd2892b1ffd801e96425831ff2f
Gummi <brimir27@outlook.com>
2020-10-01 19:54:12 +0000

    Um okkur síðan var búinn til. Öll gögn um starfsmenn eru núna geymd á json en það er alveg hægt að uppfæra það. Verkefni eru langt kominn. Búinn að stilla up html síðunum og for lykkjunum sem fara í gegnum þær. Varist samt os þar sem það getur ekki virkað á netinu

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, url_for
 from flask_assets import Environment, Bundle
 import os
+import json
 
 app = Flask(__name__)
 
 assets = Environment(app)
 assets.url = app.static_url_path
-scss = Bundle('footer.scss', 'header.scss', 'main.scss', 'project.scss', filters='pyscss', output='all.css')
+scss = Bundle('about.scss', 'footer.scss', 'header.scss', 'main.scss', 'project.scss', filters='pyscss', output='all.css')
 assets.register('scss_all', scss)
 
 session = {} 
@@ -14,24 +15,26 @@ session = {}
 def home():
     return render_template("home.html")
 
-    if request.method == 'POST':
-        session['name'] = request.form.get('Name')
-        session['email'] = request.form.get('Email')
-        session['subject'] = request.form.get('Subject')
-        session['messege'] = request.form.get('Messege')
-        print(session)  
-
 @app.route("/projects")
 def projects():
+
     projectsText = []
     for filename in os.listdir("templates/projectsTpl/"):
         if filename.split('.')[-1] == 'tpl':
             projectsText.append(filename.split('.')[0])
+
     return render_template("projects.html", projects=projectsText)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    with open("static/database/personnel.json", 'r') as file:
+        personnel = json.load(file)
+
+    for x in personnel:
+        if x["middleName"] == None:
+            x["middleName"] = ""
+
+    return render_template("about.html", personnel=personnel)
 
 if __name__ == '__main__':
     os.chdir("/srv/github/")
