bca75a67b66464e3764fdc29e58880e1fd4f302c
Gummy27 <brimir27@outlook.com>
2020-11-03 13:47:15 +0000

    Toggle dropdown er útfært. Eina eftir er að animatea hana

--- static/scripts/displayingVideosInBlock.js
+++ static/scripts/displayingVideosInBlock.js
@@ -3,15 +3,26 @@ function resizeVideoPreview() {
     let windowsWidth = document.querySelector("header").offsetWidth;
     let videoWidth;
 
-    if(windowsWidth > 1440) {
-        videoWidth = (window.innerWidth - (window.innerWidth-1440))/videoBlock.length-30;
-    } else {
-        videoWidth = window.innerWidth/videoBlock.length-30;
-    }
+    if(windowsWidth > 1054) {    
+        if(windowsWidth > 1440) {
+            videoWidth = (window.innerWidth - (window.innerWidth-1440))/videoBlock.length-30;
+        } else {
+            videoWidth = window.innerWidth/videoBlock.length-30;
+        }
 
-    for(let i = 0; i < videoBlock.length; i++) {
-        videoBlock[i].width = videoWidth;
-        videoBlock[i].height = videoWidth*0.5625;
+        for(let i = 0; i < videoBlock.length; i++) {
+            videoBlock[i].style.width = videoWidth+"px";
+            videoBlock[i].style.height = videoWidth*0.5625+"px";
+        }
+    } else {
+        console.log("this is kinda owrkin!");
+        for(let i = 0; i < videoBlock.length; i++) {
+            videoWidth = window.innerWidth-30;
+            console.log("Yes!");
+            videoBlock[i].style.width = videoWidth + "px";
+            videoBlock[i].style.height = videoWidth*0.5625 + "px";
+            console.log(videoBlock[i].style.width);
+        }
     }
 }
 
